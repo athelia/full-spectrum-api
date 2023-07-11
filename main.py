bdf090eeae50f77716bb0748adcb8edf62513b2b
Athelia Crosmun <hi@athelia.codes>
2023-07-11 17:00:57 -0400

    Stub routes

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from model import app, db
 
-def foo():
-    pass
+
+@app.route("/")
+def index():
+    return f"welcome to full spectrum eggs"
+
+
+@app.route("/stock", methods=["GET"])
+def get_stock():
+    return
+
+
+#
+# if __name__ == "__main__":
+#
