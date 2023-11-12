4480c5641be5e10c809352c9dedf8061fc02f309
Athelia Crosmun <hi@athelia.codes>
2023-11-12 01:53:17 -0500

    WIP SQLAlchemy tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,27 @@
 import unittest
 
-from api.model import AbstractIngredient
+from api.model import AbstractIngredient, app, connect_to_db, db, Product
 
 
 class TestModels(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = app.test_client()
+        app.config["TESTING"] = True
+        connect_to_db(app, "postgresql:///fullspectrum-test")
+        db.create_all()
+
+    def tearDown(self) -> None:
+        pass
+
+    def sample_product(self):
+        product = Product(name="")
+        db.session.add(product)
+        db.session.commit()
+
+    def test_product(self):
+        self.assertEqual(Product.query.get().name, "")  # FIXME
+
     def test_ingredient(self):
         flour = AbstractIngredient(name="flour")
+        self.assertEqual(flour.name, "flour")
+        self.assertIsInstance(flour, AbstractIngredient)
